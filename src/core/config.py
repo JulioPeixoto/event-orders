eb3ed2a34472c47ce5ace46425c560938f687da7
Julio Rafael <juliorafaelnft@hotmail.com>
2025-11-18 23:12:49 -0300

    init

---
+++ src/core/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: str = "sqlite:///./db.sqlite3"
+    SECRET_KEY: str = "super-secret-key"
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
---
+++ src/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from event-orders!")
+
+
+if __name__ == "__main__":
+    main()
