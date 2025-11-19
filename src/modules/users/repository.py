05acd3c222f9ca9aaf694f83ad750ae09d6d6ecb
Julio Rafael <juliorafaelnft@hotmail.com>
2025-11-19 00:37:41 -0300

    user repo

--- src/modules/users/repository.py
+++ src/modules/users/repository.py
@@ -1,7 +1,39 @@
+from typing import Optional, List
+from sqlmodel import Session, select
 from src.modules.users.models import User
 
 
 class UserRepository:
+    def __init__(self, session: Session):
+        self.session = session
 
-    def get_by_id(self, id: int) -> User:
-        pass
+    def get_by_id(self, user_id: int) -> Optional[User]:
+        return self.session.get(User, user_id)
+
+    def get_by_email(self, email: str) -> Optional[User]:
+        statement = select(User).where(User.email == email)
+        return self.session.exec(statement).first()
+
+    def get_by_username(self, username: str) -> Optional[User]:
+        statement = select(User).where(User.username == username)
+        return self.session.exec(statement).first()
+
+    def get_all(self, skip: int = 0, limit: int = 100) -> List[User]:
+        statement = select(User).offset(skip).limit(limit)
+        return self.session.exec(statement).all()
+
+    def create(self, user: User) -> User:
+        self.session.add(user)
+        self.session.commit()
+        self.session.refresh(user)
+        return user
+
+    def update(self, user: User) -> User:
+        self.session.add(user)
+        self.session.commit()
+        self.session.refresh(user)
+        return user
+
+    def delete(self, user: User) -> None:
+        self.session.delete(user)
+        self.session.commit()
